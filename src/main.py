d68b32e9878ce8f410607824a4abbcbdb74f98e7
SimonStnn <simon.stijnen.23@gmail.com>
2024-01-19 14:34:17 +0100

    Resolve pylint C0114 warnings

--- src/main.py
+++ src/main.py
@@ -4,37 +4,37 @@ import window
 if __name__ == "__main__":
     display = window.Display()
 
-    padding = 20
-    vertical_padding = 20
-    width = 80
-    height = 80
-    columns = 4
+    PADDING_X = 20
+    PADDING_Y = 20
+    WIDTH = 80
+    HEIGHT = 80
+    COLS = 4
 
     for i, sound in enumerate(display.soundboard.sounds):
-        column = i % columns
-        row = i // columns
-        x = (2 * padding) + (column * padding) + (column * width)
-        y = (2 * vertical_padding) + (row * vertical_padding) + (row * height)
+        column = i % COLS
+        row = i // COLS
+        x = (2 * PADDING_X) + (column * PADDING_X) + (column * WIDTH)
+        y = (2 * PADDING_Y) + (row * PADDING_Y) + (row * HEIGHT)
 
         button = window.Button(
             x,
             y,
-            width,
-            height,
+            WIDTH,
+            HEIGHT,
             sound[0].upper() + sound[1:3].lower(),
             on_click_sound=sound,
         )
 
         display.add_button(button)
 
-    total_width = (columns * width) + (3 * padding) + (columns * padding)
+    total_width = (COLS * WIDTH) + (3 * PADDING_X) + (COLS * PADDING_X)
     total_height = (
-        ((len(display.soundboard.sounds) + columns - 1) // columns * height)
+        ((len(display.soundboard.sounds) + COLS - 1) // COLS * HEIGHT)
         + (
-            ((len(display.soundboard.sounds) + columns - 1) // columns - 1)
-            * vertical_padding
+            ((len(display.soundboard.sounds) + COLS - 1) // COLS - 1)
+            * PADDING_Y
         )
-        + (4 * vertical_padding)
+        + (4 * PADDING_Y)
     )
 
     display.resize(total_width, total_height)
